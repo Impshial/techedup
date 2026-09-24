9e65c8ed0f4aa6a6612af61c94e2166b01ec38a4
impshial <impshial@gmail.com>
2026-09-24 13:08:52 -0400

    Release build-list HUD with gameplay and cursor modes

--- tools/test_build_list_mod.py
+++ tools/test_build_list_mod.py
@@ -10,7 +10,7 @@ guava=build.LIB/'com/google/guava/guava/14.0/guava-14.0.jar'
 cp=str(build.CLASSES)+';'+build.CP+';'+str(guava)
 build.run(build.JDK/'javac.exe','-encoding','UTF-8','-source','7','-target','7','-cp',cp,'-d',output,*sorted((ROOT/'build-list-mod/test/techit').rglob('*.java')))
 build.run(build.JDK/'java.exe','-cp',str(output)+';'+cp,'techit.buildlist.BuildListTest',output,output/'from-calculator.techit.json')
-with zipfile.ZipFile(ROOT/'build-list-mod/techit-build-list-0.2.2.jar') as jar:
+with zipfile.ZipFile(ROOT/'build-list-mod/techit-build-list-0.3.0.jar') as jar:
     assert all(n.startswith('techit/buildlist/') or n=='mcmod.info' for n in jar.namelist()),'Third-party classes leaked into distribution'
     for name in jar.namelist():
         if name.endswith('.class'):assert int.from_bytes(jar.read(name)[6:8],'big')==51,'Requires Java newer than 7'
